c5e638dbac632ad3fb441da6a2985b9d82caa439
A Tuan Media <tuan04112005@gmail.com>
2025-12-12 03:19:42 +0700

    Done

--- js/main.js
+++ js/main.js
@@ -58,26 +58,26 @@ function setupFormSubmit() {
 
 // ================== PRODUCT DATA ==================
 const existingProducts = [
-    { id: 1, name: "LED Trụ Nhựa", img: "images/sp9.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn ngoài trời" },
-    { id: 2, name: "LED Trụ Kim Cương", img: "images/sp16.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn ngoài trời" },
-    { id: 3, name: "Đèn LED Downlight", img: "images/sp11.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn trong nhà" },
-    { id: 4, name: "Đèn Tích Điện Sạc Ngoài", img: "images/sp14.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn khẩn cấp" },
-    { id: 5, name: "Đèn Pha LED", img: "images/sp4.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn trong nhà" },
-    { id: 6, name: "Đèn LED Tích Điện", img: "images/sp12.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn công nghiệp" },
-    { id: 7, name: "Đèn LED DC", img: "images/sp13.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn văn phòng" },
-    { id: 8, name: "Đèn LED Downlight Đế Liền", img: "images/sp15.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn công nghiệp" },
-    { id: 9, name: "Đèn LED Ốp Trần", img: "images/sp10.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn ngoài trời" },
-    { id: 10, name: "Ấm Thuỷ Tinh Siêu Tốc", img: "images/sp1.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Thuỷ Tinh Siêu Tốc" },
-    { id: 11, name: "Ấm Thuỷ Tinh Siêu Tốc", img: "images/sp2.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Thuỷ Tinh Siêu Tốc" },
-    { id: 12, name: "Ấm Thuỷ Tinh Siêu Tốc", img: "images/sp8.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Thuỷ Tinh Siêu Tốc" }
+    { id: 1, name: "LED Trụ Nhựa", img: "/images/sp9.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn ngoài trời" },
+    { id: 2, name: "LED Trụ Kim Cương", img: "/images/sp16.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn ngoài trời" },
+    { id: 3, name: "Đèn LED Downlight", img: "/images/sp11.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn trong nhà" },
+    { id: 4, name: "Đèn Tích Điện Sạc Ngoài", img: "/images/sp14.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn khẩn cấp" },
+    { id: 5, name: "Đèn Pha LED", img: "/images/sp4.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn trong nhà" },
+    { id: 6, name: "Đèn LED Tích Điện", img: "/images/sp12.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn công nghiệp" },
+    { id: 7, name: "Đèn LED DC", img: "/images/sp13.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn văn phòng" },
+    { id: 8, name: "Đèn LED Downlight Đế Liền", img: "/images/sp15.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn công nghiệp" },
+    { id: 9, name: "Đèn LED Ốp Trần", img: "/images/sp10.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Đèn ngoài trời" },
+    { id: 10, name: "Ấm Thuỷ Tinh Siêu Tốc", img: "/images/sp1.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Thuỷ Tinh Siêu Tốc" },
+    { id: 11, name: "Ấm Thuỷ Tinh Siêu Tốc", img: "/images/sp2.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Thuỷ Tinh Siêu Tốc" },
+    { id: 12, name: "Ấm Thuỷ Tinh Siêu Tốc", img: "/images/sp8.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Thuỷ Tinh Siêu Tốc" }
 ];
 
 // Danh sách sản phẩm thêm (khi bấm "Xem thêm")
 const additionalProducts = [
-    { id: 13, name: "Ấm Siêu Tốc", img: "images/sp3.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
-    { id: 14, name: "Ấm Siêu Tốc", img: "images/sp5.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
-    { id: 15, name: "Ấm Siêu Tốc", img: "images/sp6.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
-    { id: 16, name: "Ấm Siêu Tốc", img: "images/sp7.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
+    { id: 13, name: "Ấm Siêu Tốc", img: "/images/sp3.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
+    { id: 14, name: "Ấm Siêu Tốc", img: "/images/sp5.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
+    { id: 15, name: "Ấm Siêu Tốc", img: "/images/sp6.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
+    { id: 16, name: "Ấm Siêu Tốc", img: "/images/sp7.jpg", header: "TẬP ĐOÀN RẠNG ĐÔNG", category: "Ấm Siêu Tốc" },
 
 ];
 
